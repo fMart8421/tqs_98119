9626fb042d252426e757bc96e10f9bd6c6f569f3
Fábio Martins <fabio.m@ua.pt>
2022-03-28 16:34:52 +0100

    added toJson function

--- Lab03/lab03_2/CarInformationSystem/src/main/java/tqs/lab03/CarInformationSystem/Car.java
+++ Lab03/lab03_2/CarInformationSystem/src/main/java/tqs/lab03/CarInformationSystem/Car.java
@@ -7,47 +7,57 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
-
 @Entity
-@Table(name="car")
+@Table(name = "car")
 public class Car {
-   
+
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long carId;
     @NotNull
     private String maker;
     @NotNull
     private String model;
 
-    public Car(){}
+    public Car() {
+    }
 
-    public Car(String maker, String model){
+    public Car(String maker, String model) {
         this.maker = maker;
         this.model = model;
     }
 
-    public String getMaker(){
+    public String getMaker() {
         return this.maker;
     }
-    public void setMaker(String maker){
+
+    public void setMaker(String maker) {
         this.maker = maker;
     }
-    public String getModel(){
+
+    public String getModel() {
         return this.model;
     }
-    public void setModel(String model){
+
+    public void setModel(String model) {
         this.model = model;
     }
-    public Long getCarId(){
+
+    public Long getCarId() {
         return this.carId;
     }
-    public void setCarId(Long carId){
+
+    public void setCarId(Long carId) {
         this.carId = carId;
     }
-    
 
+    public String toJson() {
+        StringBuilder sb = new StringBuilder("{")
+                .append("\"maker\":\"").append(this.maker).append("\",")
+                .append("\"model\":\"").append(this.model).append("\"")
+                .append("}");
 
-
+        return sb.toString();
+    }
 
 }
