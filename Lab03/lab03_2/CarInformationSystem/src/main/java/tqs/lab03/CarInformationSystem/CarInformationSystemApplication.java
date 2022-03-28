0542eb9567011ec64170e00f3f91120276b1b92d
Fábio Martins <fabio.m@ua.pt>
2022-03-28 15:06:41 +0100

    restarted due to commit mistake

---
+++ Lab03/lab03_2/CarInformationSystem/src/main/java/tqs/lab03/CarInformationSystem/CarInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package tqs.lab03.CarInformationSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarInformationSystemApplication.class, args);
+	}
+
+}
---
+++ Lab03/lab03_2/CarInformationSystem/src/test/java/tqs/lab03/CarInformationSystem/CarInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.lab03.CarInformationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
