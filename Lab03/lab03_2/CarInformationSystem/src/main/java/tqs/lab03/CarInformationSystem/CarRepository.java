72ab2a03d66d5112894df527fd048bf1a2675fd6
Fábio Martins <fabio.m@ua.pt>
2022-03-28 16:43:45 +0100

    created repository

---
+++ Lab03/lab03_2/CarInformationSystem/src/main/java/tqs/lab03/CarInformationSystem/CarRepository.java
@@ -0,0 +1,9 @@
+package tqs.lab03.CarInformationSystem;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CarRepository extends JpaRepository<Car, Long> {
+    
+}
