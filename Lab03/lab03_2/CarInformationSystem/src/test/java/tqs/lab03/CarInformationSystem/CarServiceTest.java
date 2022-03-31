3f437a4bbcf12983f05c27324ea720f13b786c21
Fábio Martins <fabio.m@ua.pt>
2022-03-31 19:29:51 +0100

    added more tests

--- Lab03/lab03_2/CarInformationSystem/src/test/java/tqs/lab03/CarInformationSystem/CarServiceTest.java
+++ Lab03/lab03_2/CarInformationSystem/src/test/java/tqs/lab03/CarInformationSystem/CarServiceTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.internal.verification.VerificationModeFactory;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
@@ -28,24 +30,54 @@ public class CarServiceTest {
         Car ibiza = new Car((long)0, "Seat", "Ibiza");
         Car golf = new Car((long)1,"Volkswagen", "Golf");
         Car tesla = new Car((long)2,"Tesla", "S90D");
+        Car punto = new Car((long)3,"Fiat", "Punto");
 
-        List<Car> allCars = Arrays.asList(ibiza,golf,tesla);
+        List<Car> allCars = Arrays.asList(ibiza,golf,tesla, punto);
 
         when(carRepository.findById(ibiza.getCarId())).thenReturn(Optional.of(ibiza));
+        when(carRepository.findById((long)999)).thenReturn(Optional.empty());
         when(carRepository.findAll()).thenReturn(allCars);
-        when(carRepository.findById((long)1)).thenReturn(Optional.empty());
 
     }
 
     @Test
-    public void whenSearchExistingId_thenReturnCarWithId(){
+    public void whenSearchExistingId_thenCarShouldBeFound(){
         long id =  0;
         Car found = carManagerService.getCarById(id);
+
+        verifyFindByIdIsCalledOnce();
 
         assertThat(found.getCarId()).isEqualTo(id);
     }
 
+    @Test
+    public void whenSearchNonExistingId_thenCarShouldBeFound(){
+        Car found = carManagerService.getCarById(999L);
+        assertThat(found).isNull();
+    }
 
+    @Test
+    public void whenGiven4Cars_whenGetAll_thenReturn4Cars(){
+        Car ibiza = new Car((long)0, "Seat", "Ibiza");
+        Car golf = new Car((long)1,"Volkswagen", "Golf");
+        Car tesla = new Car((long)2,"Tesla", "S90D");
+        Car punto = new Car((long)3,"Fiat", "Punto");
+
+        List<Car> allCars = carManagerService.getAllCars();
+
+        verifyFindAllIsCalledOnce();
+
+        assertThat(allCars).hasSize(4).extracting(Car::getCarId).contains(ibiza.getCarId(), golf.getCarId(), tesla.getCarId(), punto.getCarId());
+    }
+
+
+
+    private void verifyFindByIdIsCalledOnce(){
+        Mockito.verify(carRepository, VerificationModeFactory.times(1)).findById(Mockito.anyLong());
+    }
+    private void verifyFindAllIsCalledOnce(){
+        Mockito.verify(carRepository, VerificationModeFactory.times(1)).findAll();
+    }
 
 
 }
