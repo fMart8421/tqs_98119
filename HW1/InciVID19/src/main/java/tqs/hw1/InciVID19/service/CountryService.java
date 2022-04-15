d8b28154a9ab80e0917e8a3dc816843c7784015f
Fábio Martins <fabio.m@ua.pt>
2022-04-15 18:27:19 +0100

    service template

--- HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/service/CountryService.java
+++ HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/service/CountryService.java
@@ -1,4 +1,34 @@
 package tqs.hw1.InciVID19.service;
 
+import tqs.hw1.InciVID19.model.Country;
+
+import java.util.List;
+
+
+/**
+ * This is the class that commutes with the external API.<br/>
+ * All the requests are done within this class. The methods are divided in two cases, the <strong><i>WithoutParameters</i></strong> and the <strong><i>Default</i></strong>, which is with parameters. The WithoutParameters methods are used to get Country information to be displayed like an autocomplete, for example.
+* */
 public class CountryService {
+
+
+    public List<Country> getCountriesWithoutParameters(){
+        return null;
+    }
+
+    public Country getCountryByNameDefault(){
+        return null;
+    }
+
+
+    protected List<Country> fetchApi(String name, boolean parameters){
+        return null;
+    }
+
+    protected String createUrl(){
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString();
+    }
+
 }
