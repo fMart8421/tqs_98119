d6fa2e40939da844fb91c4725f58922b102ba44b
Fábio Martins <fabio.m@ua.pt>
2022-04-15 17:28:06 +0100

    created api

---
+++ HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/InciVid19Application.java
@@ -0,0 +1,13 @@
+package tqs.hw1.InciVID19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InciVid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InciVid19Application.class, args);
+	}
+
+}
---
+++ HW1/InciVID19/src/test/java/tqs/hw1/InciVID19/InciVid19ApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.hw1.InciVID19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InciVid19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
