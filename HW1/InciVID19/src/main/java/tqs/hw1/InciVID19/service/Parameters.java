df2b771262ec7209e717e8402f26f74df64b675d
Fábio Martins <fabio.m@ua.pt>
2022-04-15 18:28:16 +0100

    minor changes

--- HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/service/Parameters.java
+++ HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/service/Parameters.java
@@ -2,10 +2,10 @@ package tqs.hw1.InciVID19.service;
 
 public enum Parameters {
     Country_Region
-, Confirmed
-, Deaths
-, Recovered
-, Active
-, Incident_Rate
-, Last_Update
+,   Confirmed
+,   Deaths
+,   Recovered
+,   Active
+,   Incident_Rate
+,   Last_Update
 }
