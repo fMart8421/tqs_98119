61e3a442e73878bd9591b68a9511215a1eefabbf
Fábio Martins <fabio.m@ua.pt>
2022-04-16 20:54:24 +0100

    implemented fetchAPI and createUrl

--- HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/service/CountryService.java
+++ HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/service/CountryService.java
@@ -1,5 +1,7 @@
 package tqs.hw1.InciVID19.service;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 import tqs.hw1.InciVID19.InciVid19Application;
 import tqs.hw1.InciVID19.model.Country;
@@ -10,7 +12,9 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.logging.Logger;
 
 @Service
@@ -34,6 +38,7 @@ public class CountryService {
 
 
     protected List<Country> fetchApi(String url) throws IOException, InterruptedException {
+        List<Country> countryList = new ArrayList<>();
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder(
                 URI.create(url)
@@ -41,11 +46,50 @@ public class CountryService {
                 .build();
         HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         String body = response.body();
-        return null;
+
+        JSONObject jsonBody = new JSONObject(body);
+        JSONArray features = jsonBody.getJSONArray("features");
+
+        for(int i=0; i<features.length();i++){
+            JSONObject countryAttributes = features.getJSONObject(i).getJSONObject("attributes");
+            JSONObject countryCoordinates = features.getJSONObject(i).getJSONObject("geometry");
+            countryList.add(
+              new Country(
+                      countryAttributes.getString("Country_Region"),
+                      countryCoordinates.getDouble("x"),
+                      countryCoordinates.getDouble("y"),
+                      countryAttributes.getInt("Confirmed"),
+                      countryAttributes.getInt("Deaths"),
+                      countryAttributes.getInt("Recovered"),
+                      countryAttributes.getInt("Active"),
+                      countryAttributes.getDouble("Incident_Rate"),
+                      countryAttributes.getLong("Last_Update")
+                      )
+            );
+        }
+
+        return countryList;
     }
 
-    protected String createUrl(String name, boolean parameters){
-        StringBuilder sb = new StringBuilder();
+
+    protected String createUrl(String name, List<String> parameters){
+        StringBuilder sb = new StringBuilder("https://services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases2_v1/FeatureServer/2/query?where=");
+        if(name.equals("")){
+            sb.append("1%3D1");
+        } else {
+            sb.append("Country_Region%20%3D%20'")
+                    .append(name.replace(" ", "%20").toUpperCase(Locale.ROOT))
+                    .append("'")
+            ;
+        }
+        StringBuilder params = new StringBuilder();
+        for (int i = 0; i < parameters.size(); i++) {
+            if (i==parameters.size()-1) params.append(parameters.get(i));
+            else params.append(parameters.get(i)).append(",");
+        }
+
+        sb.append(params)
+        .append("&outSR=4326&f=json");
 
         return sb.toString();
     }
