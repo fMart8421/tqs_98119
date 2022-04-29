048d10da4089f546414ce812744ac8a724b7b225
Fábio Martins <fabio.m@ua.pt>
2022-04-29 19:30:58 +0100

    changed method's name

--- HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/controller/CountryController.java
+++ HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/controller/CountryController.java
@@ -2,14 +2,11 @@ package tqs.hw1.InciVID19.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tqs.hw1.InciVID19.model.Country;
 import tqs.hw1.InciVID19.service.CountryService;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 public class CountryController {
@@ -23,7 +20,7 @@ public class CountryController {
     }
 
     @GetMapping("/{country}/{date}")
-    public Country getCountryWithDate(@PathVariable(value="country")String country, @PathVariable(value="date") String date) throws IOException{
+    public Country getCountryByDate(@PathVariable(value="country")String country, @PathVariable(value="date") String date) throws IOException{
         return countryService.getCountryByNameAndDay(country, date);
     }
 
