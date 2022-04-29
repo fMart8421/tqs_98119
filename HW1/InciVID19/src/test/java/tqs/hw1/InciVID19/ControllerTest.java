d0d48ff3c76d82debf0ff4ded9e1cfff97b36506
Fábio Martins <fabio.m@ua.pt>
2022-04-29 19:32:48 +0100

    controller tests passed

---
+++ HW1/InciVID19/src/test/java/tqs/hw1/InciVID19/ControllerTest.java
@@ -0,0 +1,57 @@
+package tqs.hw1.InciVID19;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import tqs.hw1.InciVID19.controller.CountryController;
+import tqs.hw1.InciVID19.model.Country;
+import tqs.hw1.InciVID19.service.CountryService;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+@WebMvcTest(CountryController.class)
+public class ControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CountryService countryService;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    void whenGetCountry_thenRetrieveCountryLatestInfo() throws Exception{
+        Country portugal = new Country("Portugal", "Europe", 0, 0, 3791744, 0, 0, 22162, "2022-04-29");
+
+        when(countryService.getCountryLatest("portugal")).thenReturn(portugal);
+
+        mockMvc.perform(get("/{country}", "portugal").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.name", is("Portugal")));
+    }
+
+    @Test
+    void whenGetCountryByDate_thenRetrieveCountryInfoForDate() throws Exception {
+        Country portugal = new Country("Portugal", "Europe", 0, 482801, 3380263, 2876177, 0, 21285, "2022-03-15");
+
+        when(countryService.getCountryByNameAndDay("portugal", "2022-03-15")).thenReturn(portugal);
+
+        mockMvc.perform(get("/{country}/{date}", "portugal", "2022-03-15").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(jsonPath("$.name", is("Portugal"))).andExpect(jsonPath("$.day", is("2022-03-15")));
+
+    }
+
+
+}
