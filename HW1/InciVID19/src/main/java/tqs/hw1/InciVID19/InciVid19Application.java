fef3097bc27ff8e034595237e9aaba3d1ee843dc
Fábio Martins <fabio.m@ua.pt>
2022-04-29 19:31:05 +0100

    added cors

--- HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/InciVid19Application.java
+++ HW1/InciVID19/src/main/java/tqs/hw1/InciVID19/InciVid19Application.java
@@ -2,12 +2,29 @@ package tqs.hw1.InciVID19;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class InciVid19Application {
+public class InciVid19Application  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InciVid19Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*")
+						.allowedMethods("GET", "POST", "DELETE", "PUT")
+						.allowedHeaders("Content-Type", "Access-Control-Allow-Origin")
+				;
+
+			}
+		};
+	}
 }
