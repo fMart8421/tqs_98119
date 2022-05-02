5c1c4ae0aca5fe6641cf0aa5e9f2507efee542b5
Fábio Martins <fabio.m@ua.pt>
2022-05-02 16:38:10 +0100

    adapted to new methods

--- HW1/InciVID19/src/test/java/tqs/hw1/InciVID19/ServiceTest.java
+++ HW1/InciVID19/src/test/java/tqs/hw1/InciVID19/ServiceTest.java
@@ -16,6 +16,8 @@ import tqs.hw1.InciVID19.cache.CountryCache;
 import tqs.hw1.InciVID19.model.Country;
 import tqs.hw1.InciVID19.service.CountryService;
 
+import java.util.Locale;
+
 @ExtendWith(MockitoExtension.class)
 public class ServiceTest {
 
@@ -33,16 +35,16 @@ public class ServiceTest {
     public void whenGetCountryLatest_cacheShouldBeAccessed(){
         Country portugalLatest = new Country("Portugal", "Europe", 0, 0, 3791744, 0, 0, 22162, "2022-04-29");
         when(countryCache.get("portugal")).thenReturn(portugalLatest);
-        assertEquals(portugalLatest, countryService.getCountryByNameAndDay("portugal", "2022-03-15"));
+        assertEquals(portugalLatest, countryService.getCountryLatest("portugal"));
         verify(countryCache, VerificationModeFactory.times(1)).get("portugal");
     }
 
     @Test
     public void whenGetCountryByNameAndDate_cacheShouldBeAccessed(){
         Country portugal = new Country("Portugal", "Europe", 0, 482801, 3380263, 2876177, 0, 21285, "2022-03-15");
-        when(countryCache.get("portugal")).thenReturn(portugal);
+        when(countryCache.get("portugal2022-03-15")).thenReturn(portugal);
         assertEquals(portugal, countryService.getCountryByNameAndDay("portugal", "2022-03-15"));
-        verify(countryCache, VerificationModeFactory.times(1)).get("portugal");
+        verify(countryCache, VerificationModeFactory.times(1)).get(portugal.getName().toLowerCase(Locale.ROOT)+portugal.getDay());
     }
 
     @Test
@@ -51,8 +53,8 @@ public class ServiceTest {
         Country usa = new Country("USA", "United-States", 0, 482801, 3380263, 2876177, 0, 21285, "2022-03-15");
         when(countryCache.get("portugal")).thenReturn(portugal);
         when(countryCache.get("usa")).thenReturn(usa);
-        assertEquals(portugal, countryService.getCountryByNameAndDay("portugal", "2022-03-15"));
-        assertEquals(usa, countryService.getCountryByNameAndDay("usa", "2022-03-15"));
+        assertEquals(portugal, countryService.getCountryLatest("portugal"));
+        assertEquals(usa, countryService.getCountryLatest("usa"));
         verify(countryCache, VerificationModeFactory.times(1)).get("portugal");
         verify(countryCache, VerificationModeFactory.times(1)).get("usa");
     }
